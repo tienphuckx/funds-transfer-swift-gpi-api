e6c12944be27f706ed03184510d7e87e6ac9c2aa
tienphuckx <tienphuckx@gmail.com>
2025-02-07 09:44:37 +0700

    init Swift gpi project

---
+++ swiftgpi/src/main/java/com/tienphuckx/swiftgpi/SwiftgpiApplication.java
@@ -0,0 +1,13 @@
+package com.tienphuckx.swiftgpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftgpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftgpiApplication.class, args);
+    }
+
+}
---
+++ swiftgpi/src/test/java/com/tienphuckx/swiftgpi/SwiftgpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienphuckx.swiftgpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftgpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
